94f6edf0c6181dce10c7a219b22a5ba8077ad15c
ansh-ankul <57483078+ansh-ankul@users.noreply.github.com>
2020-08-28 16:05:08 +0530

    third commit

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={	
+			hasError:false
+		}
+	}
+
+	componentDidCatch(error,info) {
+		this.setstate({hasError:true})
+	}
+
+	render(){
+		if(this.state.hasError){
+			return <h1>OOps Error </h1>
+		}
+		return this.props.children
+	}
+}
+
+export default ErrorBoundary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends React.Component {
@@ -48,7 +49,9 @@ class App extends React.Component {
 					<h1 className='f1'> RoboFriends </h1>
 					<SearchBox searchChange={this.onSearchChange}/>
 					<Scroll>
-						<CardList robots={filteredRobots}/>
+						<ErrorBoundary>
+							<CardList robots={filteredRobots}/>
+						</ErrorBoundary>	
 					</Scroll>
 				</div>
 			);
